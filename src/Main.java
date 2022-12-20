d0d813fad30f02df85d3dcc3b65d7d68523f5b36
Mandal <pogo591998@gmail.com>
2022-12-20 18:06:02 +0530

    Second commit

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,13 @@
+/*In this project User will enjoy to find the exact number.
+When s/he will tire to find out the number, s/he will give negative number.
+*/
 import java.sql.SQLOutput;
 import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
         // MINI PROJECT
+
         Scanner sc = new Scanner(System.in);
         int myNumber = (int) (Math.random() * 100);
 
